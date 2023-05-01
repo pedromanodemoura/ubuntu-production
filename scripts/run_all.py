ebe091659ba0418056194e834620008a80385ba7
Pedro Mano (Home) <pedrohmanodemoura@gmail.com>
2023-04-30 21:47:39 -0400

    updating run_all and linkedin reqs

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -4,12 +4,11 @@ import os
 
 start_dir = os.path.dirname(os.path.abspath(__file__))
 
-projects = subfolders = [f.path.split('\\')[-1] for f in os.scandir(start_dir) if f.is_dir()]
-
-print(projects)
+projects = [f.path.split('\\')[-1] for f in os.scandir(start_dir) if f.is_dir()]
 
 # In[2]:
 for project in projects:
+    print(f"Running {project}")
     if os.name == 'nt':
         os.system(f"cd {project} && env\\Scripts\\activate && python run_windows.py")
     elif os.name == 'posix':
